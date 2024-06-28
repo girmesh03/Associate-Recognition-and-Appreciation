310a16466ec7c73a0b53df3647ef11b6f75fa6b1
girmesh03 <girmachewzewdie03@gmail.com>
2024-06-28 20:52:59 +0300

    - index.js: export default: Navigations, publicNavbar and optionSelelctField

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,2 +1,6 @@
 export { default as Navbar } from "./navbar/Navbar";
 export { default as NavDrawer } from "./navDrawer/NavDrawer";
+export { default as Navigations } from "./navDrawer/Navigations";
+export { default as PublicNavbar } from "./publicNavbar/PublicNavbar";
+
+export { default as OptionSelectField } from "./custom/formField/OptionSelectField";
